1338f00bb95411ddcdfd3ccd18364069b44e6920
Jason R. Jones <slightlynybbled@gmail.com>
2019-08-13 22:05:30 -0400

    Automated add of nojekyll file

--- postprocess.py
+++ postprocess.py
@@ -12,3 +12,8 @@ source_path.rename('docs')
 
 build_path = Path('build')
 shutil.rmtree(build_path)
+
+nojekyll_path = docs_path / '.nojekyll'
+with open(nojekyll_path, 'w') as f:
+    f.write('')
+print(nojekyll_path)
